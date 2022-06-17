0301f707375ec4cf125ad439f59f40b025891163
Elaine Ferreira <elaineferreira.dev@gmail.com>
2022-06-17 16:42:02 -0300

    Cria pasta Estruturas condicionais

---
+++ Módulo III - JavaScript/Estruturas condiciionais/scripts.js
@@ -0,0 +1,77 @@
+// ESTRUTURA DE DECISÃO
+
+// IF, ELSE, ELSE IF
+
+// var jogador1 = 1;
+// var jogador2 = 0;
+// var placar;
+
+// if(jogador1 > 0){
+//     console.log('Jogador 1 marcou ponto')
+// } else if(jogador2 >0){
+//     console.log('Jogador 2 marcou ponto')
+// } else {
+//     console.log('ninguém marcou ponto')
+// }
+
+// NINHO DE IF
+
+// if(jogador1 = -1){
+//     if(jogador1 > 0){
+//         console.log('Jogador 1 marcou ponto')
+//     } else if(jogador2 >0){
+//         console.log('Jogador 2 marcou ponto')
+//     } else {
+//         console.log('ninguém marcou ponto')
+//     }
+// }
+
+// IF TERNÁRIO
+
+// jogador1 > 0 ? console.log('marcou ponto') : console.log('não marcou ponto')
+
+// SWITCH/CASE
+
+// switch (placar){
+//     case placar = jogador1 > jogador2:
+//         console.log('Jogador 1 ganhou');
+//         break;
+//     case placar = jogador2 > jogador1:
+//         console.log('Jogador 2 ganhou');
+//         break;
+//     default:
+//         console.log('Ninguém ganhou');
+// }
+
+// LAÇOS DE REPETIÇÃO
+// var array = ['valor1', 'valor2', 'valor3']
+
+// FOR (retorna os indices)
+// for(let i = 0; i < array.length; i++){
+//     console.log(i)
+// }
+
+// FOR IN (retorna os indices)
+// for(let i in array){
+//     console.log(i)
+// }
+
+// FOR OF (retora os valores)
+// for(i of array){
+//     console.log(i)
+// }
+
+// WHILE
+// var a = 0;
+// while (a < 10){
+//     a++;
+//     console.log(a);
+// }
+
+// DO WHILE
+// var a = 0;
+
+// do{
+//     a++;
+//     console.log(a);
+// }while(a < 10)
