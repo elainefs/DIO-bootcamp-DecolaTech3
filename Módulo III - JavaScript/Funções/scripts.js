32bb56745dacc2596d919aef3259a2acec5e22b6
Elaine Ferreira <elaineferreira.dev@gmail.com>
2022-06-18 17:11:03 -0300

    Cria funções básicas da calculadora JavaScript
    Apesar das configurações estarem iguais a da professora,
    a calculadora não está funcionado corretamente.

---
+++ Módulo III - JavaScript/Funções/scripts.js
@@ -0,0 +1,37 @@
+// function minhaPrimeiraFuncao (){
+//     console.log("Olá, mundo!")
+// }
+
+// minhaPrimeiraFuncao()
+
+// function minhaFuncaoComParametros (primeiro, segundo){
+//     console.log(primeiro, segundo)
+// }
+
+// minhaFuncaoComParametros('Olá,', 'mundo!')
+
+// EXPRESSÕES DE FUNÇÕES
+// com nomeação da função
+// var funcao = function funcao(){
+//     console.log('Isso é uma função de expressão')
+// }
+
+// funcao()
+
+// sem nomeação da função
+// var funcao = function(){
+//     console.log('Isso é uma função de expressão sem nomeação da função')
+// }
+
+// funcao()
+
+// ARROW FUNCTION
+
+// var funcao = () => {
+//     console.log('Sou uma arrow function')
+// }
+
+// funcao()
+
+// FUNÇÕES ARITMÉTICAS
+
